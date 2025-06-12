ab7f3c47ead1d825feac210073df42dc441f1eb3
Diego Dalairac <ddalairac@perceptyx.com>
2025-06-12 11:29:38 -0300

    Add doxygen to helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -5,6 +5,11 @@ import (
 	"math/rand"
 )
 
+/*
+* PrintTitle prints a title with a separator
+Parameters:
+  - message: the title to print
+*/
 func PrintTitle(message string) {
 	fmt.Println("")
 	fmt.Println("**************************************************")
@@ -13,21 +18,43 @@ func PrintTitle(message string) {
 	fmt.Println("")
 }
 
+/*
+* PrintSubTitle prints a subtitle with a separator
+Parameters:
+  - message: the subtitle to print
+*/
 func PrintSubTitle(message string) {
 	fmt.Println(" ")
 	fmt.Println(message)
 	fmt.Println("-------------------------------------------------")
 }
 
-func GetRandomNumber(min int, max int) int {
+/*
+* GetRandomNumber generates a random integer between min and max (inclusive)
+Parameters:
+  - min: the minimum value in the range
+  - max: the maximum value in the range
+
+Returns:
+  - a random integer between min and max
+*/
+func GetRandomNumber(min, max int) int {
 	return rand.Intn(max-min+1) + min
 }
 
+/*
+* GetRandomString generates a random string of specified length
+Parameters:
+  - length: the desired length of the random string
+
+Returns:
+  - a random string containing only letters (a-z, A-Z)
+*/
 func GetRandomString(length int) string {
-	letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = charset[rand.Intn(len(charset))]
 	}
 	return string(b)
 }
